912ff5c75ccb76dc649b4ccf3287ad857ac90157
linkmvv007 <linkmvv@gmail.com>
2025-10-08 14:33:07 +0300

    polly timeout

--- DataProcessorService/Program.cs
+++ DataProcessorService/Program.cs
@@ -34,7 +34,7 @@ Host.CreateDefaultBuilder(args)
             {
                 PolicyRegistryConsts.RabbitRetryKey, Policy
                     .Handle<Exception>(ex => ex is not OperationCanceledException)
-                    .WaitAndRetryAsync(3, attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)),
+                    .WaitAndRetryAsync(7, attempt => TimeSpan.FromSeconds(Math.Pow(2, attempt)),
                         (ex, ts, count, ctx) =>
                         {
                             var logger = ctx[PolicyRegistryConsts.Logger] as Microsoft.Extensions.Logging.ILogger;
